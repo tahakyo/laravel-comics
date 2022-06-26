7fa52b514b20ba74e28b6879afac7433f7ec9ede
tahakyo <ghazouani1988@email.com>
2022-06-27 00:40:05 +0200

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,25 @@
+
 <header>
-  header
+  <div class="additional-bar">
+    <div class="additional-bar__content container">
+      <a href="">dc power visa®</a>
+      <a href="">additional dc site</a>
+    </div>
+  </div>
+  <div class="menu container">
+    <div class="menu__logo">
+      <img src="{{ asset ('images/dc-logo.png') }}" alt="dc-logo">
+    </div>
+    <nav>
+      <ul>
+        <li>menu#</li>
+        <li>menu#</li>
+        <li>menu#</li>
+        <li>menu#</li>
+        <li>menu#</li>
+        <li>menu#</li>
+        <li>menu#</li>
+      </ul>
+    </nav>
+  </div>
 </header>
